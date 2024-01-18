990558855929add760e3a49cd4da7cd38a6f6f7d
Josef Rabmer <jrabmer@outlook.com>
2024-01-18 15:33:32 +0100

    Updated readme, added csv files and added method stumps in agent class

--- README.md
+++ README.md
@@ -1,16 +1,42 @@
 # putz-agent
 These are some changes
 ## State Machine
-Resets every week to keep it simple. Can be extended of course in the future. Can also do two operational modes. Simple mode where agent just tells user what to clean and schedules them for a standard day (Sunday?). If user wants to schedule different day, they can reschedule i guess. 
+Resets every week to keep it simple. Can be extended of course in the future. Can also do two operational modes. 
+Simple mode where agent just tells user what to clean and schedules them for a standard day (Sunday?). If user wants to 
+schedule different day, they can reschedule I guess. 
 
 1. Send Users notification to schedule cleaning this week (Randomise cleaning areas or do cleaning rotation ?)
-2. User sends prefered day to clean
+2. User sends preferred day to clean
 3. Agent reminds user to clean maybe the day before / on the day ?
-4. Agent reminds user if they forgot until they do it! (Could become progressively worse by insulting the user and spamming them if they do not get it done xD)
+4. Agent reminds user if they forgot until they do it! (Could become progressively worse by insulting the user and 
+spamming them if they do not get it done xD)
 5. User sends confirmation that they cleaned
 6. Agent notes this down (optional)
 
 ![State Diagram of agent](/project_documentation/putz_agent_state_diagram.drawio.png?raw=true "State Diagram")
 
 ## Penalty Cleaning
-If someone misses their cleaning task, they will receive a punishment point. These will be saved in a file. The agent then decides the assignemnt of the cleaning areas of the next week with these points in mind. This will be done in a simple manner. There are three cleaning areas. For each cleaning area, the agent will pick a random element from an array (that contains the names of all wg members). If the user a has n punishment points, then n additional a elements will be added to the array. Therefore the chance for a user to have to clean multiple areas increases depending on how many points you have. This also has the effect that in some weeks, other wg members might not have to clean at all.
+If someone misses their cleaning task, they will receive a penalty point. These will be saved in a file. The agent then 
+decides the assignment of the cleaning areas of the next week with these points in mind. This will be done in a simple 
+manner. There are three cleaning areas. For each cleaning area, the agent will pick a random element from an array 
+(that contains the names of all wg members). If the user a has n penalty points, then n additional elements will be 
+added to the array. Therefore, the chance for a user to have to clean multiple areas increases depending on how many 
+points you have. This also has the effect that in some weeks, other wg members might not have to clean at all.
+
+## File Layout
+
+The file penalty_points will have the following layout:
+
+| User | Penalties |
+|------|-----------|
+| F    | 0         |
+| M    | 0         |
+| J    | 0         |
+
+The file scheduled_dates will have the following layout:
+
+| User | Date       |
+|------|------------|
+| F    | YYYY-MM-DD |
+| M    | YYYY-MM-DD |
+| J    | YYYY-MM-DD |
--- main.py
+++ main.py
@@ -11,45 +11,74 @@ except KeyError:
 
 agent_email = "kallax.brueder.wg@gmail.com"
 
-def send_mail(receiver=agent_email, subject=" ", message=" "):
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
 
-    context = ssl.create_default_context()
+class Agent:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(agent_email, password)
+    def daily_check(self):
+        # TODO: Implement check that runs every day to decide what to do
+        pass
 
-        server.sendmail(agent_email, receiver, f'Subject: {subject} \n\n{message}')
-        server.quit()
+    def assign_people_to_cleaning_area(self):
+        # TODO: Implement assignment
+        pass
 
+    def send_mail(self, receiver=agent_email, subject=" ", message=" "):
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
 
-def get_first_text_block(email_message_instance):
-    maintype = email_message_instance.get_content_maintype()
-    if maintype == 'multipart':
-        for part in email_message_instance.get_payload():
-            if part.get_content_maintype() == 'text':
-                return part.get_payload()
-    elif maintype == 'text':
-        return email_message_instance.get_payload()
+        context = ssl.create_default_context()
 
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(agent_email, password)
 
-def receive_latest_email():
-    mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login(agent_email, password)
-    mail.list()
-    # Out: list of "folders" aka labels in gmail.
-    mail.select("inbox")  # connect to inbox.
+            server.sendmail(agent_email, receiver, f'Subject: {subject} \n\n{message}')
+            server.quit()
 
-    result, data = mail.uid('search', None, "ALL")  # search and return uids instead
-    latest_email_uid = data[0].split()[-1]
-    result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-    raw_email = data[0][1]
-    msg = email.message_from_bytes(raw_email)
+    def get_first_text_block(self, email_message_instance):
+        maintype = email_message_instance.get_content_maintype()
+        if maintype == 'multipart':
+            for part in email_message_instance.get_payload():
+                if part.get_content_maintype() == 'text':
+                    return part.get_payload()
+        elif maintype == 'text':
+            return email_message_instance.get_payload()
 
-    print(msg['Subject'])
-    print(get_first_text_block(msg))
+    def receive_latest_email(self):
+        # TODO: Change this to check if user has sent mail this past week (confirmation of cleaning)
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(agent_email, password)
+        mail.list()
+        # Out: list of "folders" aka labels in gmail.
+        mail.select("inbox")  # connect to inbox.
+
+        result, data = mail.uid('search', None, "ALL")  # search and return uids instead
+        latest_email_uid = data[0].split()[-1]
+        result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        raw_email = data[0][1]
+        msg = email.message_from_bytes(raw_email)
+
+        print(msg['Subject'])
+        print(self.get_first_text_block(msg))
+
+    def read_points_from_file(self):
+        # TODO: Implement
+        pass
+
+    def write_points_to_file(self, user_points):
+        # TODO: Implement and make sure points are written to correct spot
+        pass
+
+    def read_scheduled_dates_from_file(self):
+        # TODO: Implement
+        pass
+
+    def write_scheduled_dates_to_file(self, scheduled_dates):
+        # TODO: Implement
+        pass
 
 if __name__ == "__main__":
-    send_mail(agent_email, "github tets", "This is an email test from github actions")
-#receive_latest_email()
+    agent = Agent(agent_email, password)
+    agent.daily_check()
